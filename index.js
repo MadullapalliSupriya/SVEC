807cf5c51d8954957e40fe656ae0fd194cb8fc0c
MadullapalliSupriya <96712430+MadullapalliSupriya@users.noreply.github.com>
2022-01-01 12:15:18 +0530

    hellojan1st

---
+++ index.js
@@ -0,0 +1,479 @@
+// let name="Vamcy";
+// var age=20;
+
+// if(age>18){
+//     var name="Vamcy A";
+//     var name="dt";
+//     console.log(name);
+// }
+
+// console.log(name);
+
+//Objects
+// let employee={
+//     name:"HAsii",
+//     designation:"btech 2nd year just studing",
+//     salary:"0LPA",
+//     mobile:9875641
+// }
+
+
+// console.log()
+
+// let employees=["suppi","hasii","maggi"]
+// console.log(employees[1])
+
+//alrets
+// alert("Registration is completed successfully")
+// let num1=prompt("enter number1");
+// console.log(number1);
+// console.log("welcome");
+// console.info("for info");
+// console.warn("Check the code once");
+// console.error("name is not defined")
+
+//Spread Operator
+
+// let external_marks=[23,33,54,44,32,33]
+// let internal_marks=[12,3,14,16,17]
+
+// let results=[23,34,external_marks]
+// let final=[23,445,556,...external_marks,...internal_marks]
+// console.log(results)
+// console.log(final)
+
+//Rest parameter
+ 
+// function addition(x,y,...remainData){
+//     // console.log(x,y)
+//     console.log(x)
+//     console.log(y)
+//     console.log(remainData)
+//     console.log(typeof(remainData))
+// }
+// console.log(addition(2,3,242,423434,23423,424,234,"wewe"))
+// // addition(2,3)
+
+
+// Destructuring of Array
+
+// let a=[]
+// let employees =["Supriya","Meghana","Hasitha"]
+// for(i=0;i<3;i++){
+//      name1=employees[i];
+//      a[i]=name1;
+//     console.log(name1)
+// }
+// // console.log(employees[0])
+
+// console.log(a)
+
+// let employee={
+//     uname:"Supriya",
+//     designation:"Just ready to take degree",
+//     salary:"yet to come",
+//     moblie:823723
+// }
+
+// let { uname,designation,...remainList}=employee;
+// console.log(uname);
+// console.log(designation);
+// console.log(remainList)
+
+//Function
+
+// let demo=funciton(x,y){
+//     return x+y
+// }
+
+
+// (function(x,y){
+// y})()
+// setTimeout(function(){
+//     console.log("Welcome")
+// },3000)
+
+
+// console.log(demo(3,4))
+
+// Arrow function
+
+// let demo= (x,y)=>{
+//     return x*y
+// }
+// console.log(demo(3,4))
+
+let employees=["Siva","Hari","Ravi","Abhi"]
+
+// for(let i=0;i<employees.length;i++){
+//     console.log(employees[i])
+// }
+// for-in
+// for(let i in employees){
+//     console.log(i)
+// }
+
+//for-of
+
+// for(let i of employees){
+//     console.log(i)
+// }
+
+
+//forEach
+// employees.forEach(function(element,index){
+//     console.log(element+" Ok")
+// })
+
+
+//map function
+
+// let result=employees.map(function(element,index){
+//     return element+"k"+index
+//  })
+// console.log(employees)
+// console.log(result)
+
+
+
+
+//  let result=employees.map((element,index)=>{
+//     return element+"k"+index
+//  })
+// // console.log(employees)
+// console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
